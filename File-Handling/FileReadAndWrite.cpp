a8f0fcdfae6c6b9f11b541fc6622a5c43199e168
okkessefa <oksekesfackn53@gmail.com>
2025-07-16 18:01:05 +0300

    Add File Handling notes folder to repository

---
+++ File-Handling/FileReadAndWrite.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+
+
+int main()
+{
+    // std::fstream myFile;
+    
+    // myFile.open("Work.txt", std::ios::out); // Writing mode
+    // if(myFile.is_open())
+    // {
+    //     myFile << "Hello\n";
+    //     myFile << "I am working and this is second line\n";
+    //     myFile.close();
+    // }
+
+    //myFile.open("Work.txt", std::ios::out); // over write 
+    //if(myFile.is_open())
+    //{
+    //    myFile << "Hello again \n";
+    //    myFile << "I am working still in this line\n";
+    //    myFile.close();
+    //}
+
+    // myFile.open("Work.txt", std::ios::app); // append mode write 
+    // if(myFile.is_open())
+    // {
+    //     myFile << "Hello again \n";
+    //     myFile << "I am working still in this line\n";
+    //     myFile.close();
+    // }
+    std::fstream myFile;
+    myFile.open("Work.txt", std::ios::in); // read mode
+    if(myFile.is_open())
+    {
+        std::string line;
+        while(std::getline(myFile ,line))
+        {
+            std::cout<< line << std::endl;
+        }
+        myFile.close();
+    }
+
+    return 0;
+}
